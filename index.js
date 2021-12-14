f1670fc32cb3cbfe1cdb304054ee7bc3d3cf7fcc
nathalia <nathaliatroina@gmail.com>
2021-12-13 23:34:35 -0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ("Guadalupe", "Ollie", "Aki");
+
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+
+}
+  
+
+function countDown(positveNumber){
+    while(positveNumber >= 0){
+        console.log(positveNumber);
+        positveNumber--
+    }
+
+}
+   
